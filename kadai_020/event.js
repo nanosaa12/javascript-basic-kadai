190622e2e2f87c64d1f51f88edb28e75b893bd91
nanosaa12 <156193917+nanosaa12@users.noreply.github.com>
2024-03-23 19:59:52 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('btn');
+const letter = document.getElementById('text');
+
+  button.addEventListener('click',() => {
+
+  letter.textContent =('ボタンをクリックしました');
+})
+
